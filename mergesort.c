4c58289deb1ec6bc05c336ca79a148026bd857ea
Rinto Cherian <chrint223@gmail.com>
2025-10-19 02:44:32 +0530

    Add mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void merge(int arr[],int left,int mid,int righ){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1],R[n2];
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for(int j = 0; j < n2; j++)
+        L[j] = arr[right + j];
+    int i = 0, j = 0, k = left;
+    while(i<n1 && j<n2){
+        if(L[i] <= R[j]){
+            
+        }
+    }
+
+}
