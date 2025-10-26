e4045251a0955776f45596275461c4ac1c383456
Rinto Cherian <chrint223@gmail.com>
2025-10-26 15:54:40 +0530

    Add mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,17 +1,68 @@
 #include <stdio.h>
-void merge(int arr[],int left,int mid,int righ){
+
+void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-    int L[n1],R[n2];
-    for(int i = 0; i < n1; i++)
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
-    for(int j = 0; j < n2; j++)
-        L[j] = arr[right + j];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];  // ✅ fixed
+
     int i = 0, j = 0, k = left;
-    while(i<n1 && j<n2){
-        if(L[i] <= R[j]){
-            
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
         }
+        k++;
     }
 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;  // ✅ fixed midpoint
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the length of the array:\n");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    mergesort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
